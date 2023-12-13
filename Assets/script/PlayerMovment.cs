6915a91d7efb9423d4cd1da8c1ac6a5e6f1c9082
Dıaa Dwıkat <oava9944@gmail.com>
2023-12-13 14:44:05 +0300

    Add animation while character moves and give him the ability to jump using space key

--- Assets/script/PlayerMovment.cs
+++ Assets/script/PlayerMovment.cs
@@ -1,26 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class PlayerMovement : MonoBehaviour
+
+public class PlayerMovement2 : MonoBehaviour
 {
-public float moveSpeed = 3.5f;
-public float rotateSpeed = 100.0f;
-private Rigidbody rb;
-// Start is called before the first frame update
-void Start()
-{
-rb = GetComponent<Rigidbody>();
-}
-// Update is called once per frame
-void FixedUpdate()
-{
-Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
-Vector3 rotateY = new Vector3(0, Input.GetAxis("Mouse X") * rotateSpeed * Time.deltaTime, 0);
-if (movement != Vector3.zero)
-{
-rb.MoveRotation(rb.rotation * Quaternion.Euler(rotateY));
-}
-rb.MovePosition(rb.position + transform.forward * Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime +
-transform.right * Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime);
-}
+    public float moveSpeed = 3.5f;
+    public float rotateSpeed = 100.0f;
+    private Rigidbody rb;
+    private Animator anim;
+    private bool isJumping = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+    }
+
+    // FixedUpdate is called once per physics update
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+        Vector3 rotateY = new Vector3(0, Input.GetAxis("Mouse X") * rotateSpeed * Time.deltaTime, 0);
+
+        if (movement != Vector3.zero)
+        {
+            rb.MoveRotation(rb.rotation * Quaternion.Euler(rotateY));
+        }
+
+        rb.MovePosition(rb.position + transform.forward * Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime +
+            transform.right * Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime);
+
+        anim.SetFloat("BlendV", Input.GetAxis("Vertical"));
+        anim.SetFloat("BlendH", Input.GetAxis("Horizontal"));
+
+        if (isJumping)
+        {
+            rb.AddForce(Vector3.up * 1200 * Time.deltaTime, ForceMode.VelocityChange);
+            isJumping = false; // Reset the flag after applying the force
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump") && !isJumping)
+        {
+            isJumping = true;
+            StartCoroutine(StopJump());
+        }
+    }
+
+    IEnumerator StopJump()
+    {
+        yield return new WaitForSeconds(0.1f);
+        isJumping = false;
+    }
 }
