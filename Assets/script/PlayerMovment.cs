9bed93c8009fe60a1f12030dc71e3a4e00bbdfb0
Dıaa Dwıkat <oava9944@gmail.com>
2023-12-13 14:30:28 +0300

    editing the player movment to give him the ability to rotate the character while he moves

--- Assets/script/PlayerMovment.cs
+++ Assets/script/PlayerMovment.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 public float moveSpeed = 3.5f;
+public float rotateSpeed = 100.0f;
 private Rigidbody rb;
 // Start is called before the first frame update
 void Start()
@@ -11,10 +12,15 @@ void Start()
 rb = GetComponent<Rigidbody>();
 }
 // Update is called once per frame
-void Update()
+void FixedUpdate()
 {
-rb.MovePosition(rb.position + transform.forward * Input.GetAxis
-("Vertical") * moveSpeed * Time.deltaTime + transform.right *
-Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime);
+Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+Vector3 rotateY = new Vector3(0, Input.GetAxis("Mouse X") * rotateSpeed * Time.deltaTime, 0);
+if (movement != Vector3.zero)
+{
+rb.MoveRotation(rb.rotation * Quaternion.Euler(rotateY));
+}
+rb.MovePosition(rb.position + transform.forward * Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime +
+transform.right * Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime);
 }
 }
