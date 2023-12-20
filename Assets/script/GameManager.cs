231e62c935621e8603d25a8e836e3370ced03f49
Dıaa Dwıkat <oava9944@gmail.com>
2023-12-20 19:43:08 +0300

    Adding the the MainMenu page Ui and place the buttton and inutfiled

--- Assets/script/GameManager.cs
+++ Assets/script/GameManager.cs
@@ -2,17 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using UnityEngine.UI;
 public class GameManager : MonoBehaviourPunCallbacks
 {
+public InputField playerNickname;
+private string setName = "";
+public GameObject connecting;
 // Start is called before the first frame update
 void Start()
 {
-PhotonNetwork.AutomaticallySyncScene = true;
-PhotonNetwork.ConnectUsingSettings();
+connecting.SetActive(false);
 }
 // Update is called once per frame
-void Update()
+public void UpdateText()
 {
+setName = playerNickname.text;
+PhotonNetwork.LocalPlayer.NickName = setName;
+}
+public void EnterButton()
+{
+if (setName != "")
+{
+PhotonNetwork.AutomaticallySyncScene = true;
+PhotonNetwork.ConnectUsingSettings();
+connecting.SetActive(true);
+}
+}
+public void ExitButton()
+{
+Application.Quit();
 }
 public override void OnConnectedToMaster()
 {
@@ -23,7 +41,9 @@ public override void OnJoinedRoom()
 {
 PhotonNetwork.LoadLevel("Floor layout");
 }
-public override void OnJoinRandomFailed(short returnCode, string message)
+public override void OnJoinRandomFailed(short returnCode, string
+message)
+
 {
 PhotonNetwork.CreateRoom("Arena1");
 }
--- Assets/script/LookAt.cs
+++ Assets/script/LookAt.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
 public class LookAt : MonoBehaviour
 {
 private Vector3 worldPosition;
 private Vector3 screenPosition;
 public GameObject crosshair;
+public Text nickNameText;
 // Update is called once per frame
+private void Start(){
+    nickNameText.text = PhotonNetwork.LocalPlayer.NickName;
+}
+
 void FixedUpdate()
 {
 screenPosition = Input.mousePosition;
